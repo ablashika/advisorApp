86c03ef77df677855d3da90e7d78f5c1ace13489
ablashika <kofoworola203@gmail.com>
2022-12-12 21:33:29 +0000

    initial commit

---
+++ src/Components/AdvisoPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ".././styles.css"
+
+export default function AdvisoPage() {
+  return (
+    <div className="first-container">
+     
+       <div  className="advice-box">
+             <p>ADVICE #117</p>
+             <p>"it is easy to sit up and take notice,
+                <br></br>AdvisoPage. what's difficult is getting up and takinf action."</p>
+
+       </div>
+
+    </div>
+  )
+}
