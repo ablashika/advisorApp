dd359675d56fd6d2d3349fb3820d762a85715b93
ablashika <kofoworola203@gmail.com>
2022-12-14 17:42:51 +0000

    made an api call

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import AdvisoPage from './Components/AdvisoPage';
 
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <AdvisoPage/>  
+      <AdvisoPage quotes="it is easy to take notice"/>  
     </div>
   );
 }
