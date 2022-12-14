ce2d9c69f3b17ff0c0d7116530b1e704ac3dcbcb
ablashika <kofoworola203@gmail.com>
2022-12-14 17:43:28 +0000

    made an api call

--- src/Components/AdvisoPage.js
+++ src/Components/AdvisoPage.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import ".././styles.css"
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+
+
+
+
 
 export default function AdvisoPage() {
+
+  let myAdvice = []
+  const [advices, setAdvice] = useState(myAdvice)
+
+  useEffect (()=>{
+    getAllPost()
+  },[])
+
+
+const getAllPost = () =>{
+  axios
+  .get("https://api.adviceslip.com/advice")
+  .then((res) => {
+    // console.log(res);
+    console.log("jj",[res.data.slip]);
+    setAdvice([res.data.slip]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
+  }  
   return (
     <div className="first-container">
-     
        <div  className="advice-box">
-             <p>ADVICE #117</p>
-             <p>"it is easy to sit up and take notice,
-                <br></br>AdvisoPage. what's difficult is getting up and takinf action."</p>
+      
+      
 
+      {advices.map((users, key)=>{
+         return ( <div>
+          <p>{users.id}</p>
+          <p>{users.advice}</p>
+          
+          </div>
+)
+       })} 
        </div>
 
     </div>
